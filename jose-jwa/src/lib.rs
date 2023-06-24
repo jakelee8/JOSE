7a7e71d00ad50f35cf6682f2eda642fcafcb04f8
Trevor Gross <tmgross@umich.edu>
2023-06-23 21:31:59 -0400

    Rename `Signing::Null` to `Signing::None`
    There is no need to avoid conflict with `Option::None` if glob imports are
    avoided; "none" is a more accurate term here than "null".

--- jose-jwa/src/lib.rs
+++ jose-jwa/src/lib.rs
@@ -95,10 +95,8 @@ pub enum Signing {
     Rs512,
 
     /// No digital signature or MAC performed (Optional)
-    ///
-    /// This variant is renamed as `Null` to avoid colliding with `Option::None`.
     #[serde(rename = "none")]
-    Null,
+    None,
 }
 
 impl Signing {
