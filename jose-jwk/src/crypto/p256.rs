b542581562c43223a90f86493b50aee8f548c78d
Trevor Gross <tmgross@umich.edu>
2023-06-23 21:36:34 -0400

    Apply change from `Signing::Null` to `Signing::None`

--- jose-jwk/src/crypto/p256.rs
+++ jose-jwk/src/crypto/p256.rs
@@ -6,7 +6,7 @@
 use p256::elliptic_curve::sec1::{FromEncodedPoint, ToEncodedPoint};
 use p256::{EncodedPoint, FieldBytes, PublicKey, SecretKey};
 
-use jose_jwa::{Algorithm, Algorithm::Signing, Signing::*};
+use jose_jwa::{Algorithm, Algorithm::Signing, Signing as S};
 
 use super::Error;
 use super::KeyInfo;
@@ -23,7 +23,7 @@ impl KeyInfo for PublicKey {
     }
 
     fn is_supported(&self, algo: &Algorithm) -> bool {
-        matches!(algo, Signing(Es256))
+        matches!(algo, Signing(S::Es256))
     }
 }
 
@@ -33,7 +33,7 @@ impl KeyInfo for SecretKey {
     }
 
     fn is_supported(&self, algo: &Algorithm) -> bool {
-        matches!(algo, Signing(Es256))
+        matches!(algo, Signing(S::Es256))
     }
 }
 
